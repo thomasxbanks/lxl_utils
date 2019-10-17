e18c9e8d4a9f1d531ca45a1a15d1cfc78b4b2d13
thomasxbanks <thomasxbanks@gmail.com>
2019-10-17 15:00:18 +0100

    Adds `isDoubleTap` function

---
+++ is-double-tap/index.js
@@ -0,0 +1,15 @@
+/**
+  * @constant {Function} - Determines whether a "double tap" has been performed
+  * @param {Number} - duration - (Optional) Time in milliseconds between clicks/taps. Defaults to 500
+  * @returns {Boolean} - `true` if two or more clicks/taps occur within the duration period, otherwise false
+*/
+
+const isDoubleTap = (duration = 500) => {
+  const now = new Date().getTime();
+  const timesince = now - window.mylatesttap;
+  const isDoubleTap = ((timesince < duration) && (timesince > 0));
+  window.mylatesttap = new Date().getTime();
+  return isDoubleTap;
+};
+
+module.exports = isDoubleTap
